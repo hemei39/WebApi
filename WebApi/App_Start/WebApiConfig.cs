d9bb853738024dc10c61769072efa3eee4c485d2
DESKTOP-GIP058U\Mei <hemei39@hotmail.com>
2018-10-06 18:42:28 -0400

    Add project files.

---
+++ WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // remove null properties from the result
+            config.Formatters.JsonFormatter.SerializerSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            // Set Json format as default
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            // change format to the output to Camel Case
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+            settings.Formatting = Formatting.Indented;
+            settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+        }
+    }
+}
---
+++ WebApi/Controllers/DalController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class DalController : ApiController
+    {
+        //ado.net
+        public string connStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public DataTable GetDataTable(string spName, List<SqlParameter> sqlParams = null)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+
+                    SqlCommand cmd = new SqlCommand(spName, conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    if (sqlParams != null)
+                    {
+                        foreach (SqlParameter param in sqlParams)
+                        {
+                            cmd.Parameters.Add(param);
+                        }
+                    }
+
+                    SqlDataAdapter dad = new SqlDataAdapter(cmd);
+                    DataSet dst = new DataSet();
+                    dad.Fill(dst);
+                    return dst.Tables[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+        public HttpResponseMessage GetCategories()
+        {
+            DataTable dtbl = GetDataTable("spGetCategories");
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, dtbl);
+
+            return response;
+
+        }
+
+        public HttpResponseMessage GetProductsByCategoryID(int id)
+        {
+            List<SqlParameter> sqlParams = new List<SqlParameter>();
+
+            SqlParameter param = new SqlParameter();
+            param.ParameterName = "@CategoryID";
+            param.SqlDbType = SqlDbType.Int;
+            param.Value = id;
+
+            sqlParams.Add(param);
+
+            DataTable dtbl = GetDataTable("spGetProductsByCategoryID", sqlParams);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, dtbl);
+
+            return response;
+
+        }
+
+        //entity framework
+        public HttpResponseMessage GetProducts()
+        {
+            using (NorthwindEntities context = new NorthwindEntities())
+            {
+                List<Product> products = context.Products.ToList();
+
+                HttpResponseMessage reponse = Request.CreateResponse(HttpStatusCode.OK, products);
+
+                return reponse;
+            }
+        }
+        public HttpResponseMessage GetProductByProductID(int id)
+        {
+            using (NorthwindEntities contex = new NorthwindEntities())
+            {
+                Product product = contex.Products.Where(p => p.ProductID == id).FirstOrDefault();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, product);
+                return response;
+            }
+        }
+    }
+}
